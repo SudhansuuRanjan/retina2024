a1dd3cb2fd4517ce163c5b86b34ff56c341b7416
SudhanshuuRanjan <sudhanshuranjan2k18@gmail.com>
2024-10-31 03:12:40 +0530

    fixed bugs

--- client/src/components/HomeComponents/Gallery.jsx
+++ client/src/components/HomeComponents/Gallery.jsx
@@ -3,15 +3,13 @@ import StyledImage from "./StyledImage";
 
 const Gallery = () => {
     return (
-        <div className="py-32 bg-gradient-to-b from-black to-gray-900">
+        <div className="py-32 bg-gradient-to-b from-black to-gray-900 max-w-[100vw] overflow-hidden">
             <div className='flex items-center justify-center text-center gap-3 flex-col mb-24'>
                 <p className='lg:text-6xl md:text-6xl text-5xl font-medium fancyfont'>GALLERY</p>
             </div>
 
-            <Marquee speed={100} autoFill={true} className="lg:mb-12 md:mb-10 mb-6" pauseOnHover={true}>
-                {/* <div className="lg:m-6 md:m-4 m-3">
-                    <img src="https://picsum.photos/200/200?a=1" className="h-56 hover:scale-105 transition cursor-pointer rounded-lg " alt="logo" />
-                </div > */}
+            <div className="rotate-[5deg]">
+                <Marquee speed={100} autoFill={true} className="lg:mb-12 md:mb-10 mb-6" pauseOnHover={true}>
                 <StyledImage src="https://picsum.photos/200/200?a=1" />
                 <StyledImage src="https://picsum.photos/200/200?a=2" />
                 <StyledImage src="https://picsum.photos/200/200?a=3" />
@@ -20,7 +18,9 @@ const Gallery = () => {
                 <StyledImage src="https://picsum.photos/200/200?a=6" />
                 <StyledImage src="https://picsum.photos/200/200?a=7" />
             </Marquee>
+            </div>
 
+             <div className="rotate-[5deg]">
             <Marquee speed={100} direction="right" autoFill={true} pauseOnHover={true}>
                 <StyledImage src="https://picsum.photos/200/200?a=8" />
                 <StyledImage src="https://picsum.photos/200/200?a=9" />
@@ -30,6 +30,7 @@ const Gallery = () => {
                 <StyledImage src="https://picsum.photos/200/200?a=13" />
                 <StyledImage src="https://picsum.photos/200/200?a=14" />
             </Marquee>
+            </div>
 
         </div>
     )
--- client/src/components/HomeComponents/StyledImage.jsx
+++ client/src/components/HomeComponents/StyledImage.jsx
@@ -2,13 +2,45 @@ import "./StyledImage.css"
 
 const StyledImage = ({ src }) => {
     return (
-        <div className="lg:m-6 md:m-4 m-3">
-            <div class="image-wrapper">
-                <img src={`${src ? src : "https://picsum.photos/200/200?a=12"}`} alt="Rounded Image" class="rounded-image h-56 hover:scale-105 transition cursor-pointer rounded-lg" />
-                <div class="absolute top-right"></div>
-                <div class="absolute bottom-left"></div>
+        // <div className="lg:m-6 md:m-4 m-3">
+        //     <div class="image-wrapper">
+        //         <img src={`${src ? src : "https://picsum.photos/200/200?a=12"}`} alt="rounded-sm Image" class="rounded-sm-image h-56 hover:scale-105 transition cursor-pointer rounded-sm-lg" />
+        //         <div class="absolute top-right"></div>
+        //         <div class="absolute bottom-left"></div>
+        //     </div>
+        // </div>
+        <div>
+            <div className="flex gap-2 justify-between items-center p-1">
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+            </div>
+            <img src={`${src ? src : "https://picsum.photos/200/200?a=12"}`} alt="Image" class="h-56 hover:scale-105 transition cursor-pointer my-2 mx-2" />
+            <div className="flex gap-2 justify-between items-center p-1">
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
+                <div className="size-2.5 rounded-sm bg-white"></div>
             </div>
         </div>
+
     )
 }
 
